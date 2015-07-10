ac133f4ee707a61e35c4f69cceb33412b011de0c
antirek <serge.dmitriev@gmail.com>
2015-07-10 23:41:34 +0700

    some changes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,76 @@
 
 var owo = function () {
 
-  var Layout = function () {
+  var options = {
+    media: {
+      constraints: {
+        audio: true,
+        video: false
+      },
+      render: {
+        remote: document.getElementById('owoAudio'),
+        local: document.getElementById('owoAudio')
+      }
+    }
+  };
+
+  var userAgent;
+
+  var prepareLayout = function () {
     var component = document.createElement("div");
     component.setAttribute("id", "owoPhone");
 
+
+    var audio = document.createElement('audio');
+    audio.setAttribute("id", "owoAudio");
+
+
+    component.appendChild(audio);
     document.body.appendChild(component);
   };
 
-  var phone = function (options) {
-    Layout();
+  var initSipJs = function (config1) {
+
+    //@todo: get options from remote side
+
+    var config = {
+      uri: '1004@109.234.38.141',
+      wsServers: 'ws://109.234.38.141:5066',
+      authorizationUser: '1004',
+      password: '1234',
+      register: true,
+      log: {
+        builtinEnabled: true,
+        level: 'debug'
+      }
+    };
+
+    userAgent = new SIP.UA(config);    
+  };
+
+  var phone = function (config) {
+    prepareLayout();
+    initSipJs(config);
+  };
+
+  var call = function (target) {
+    userAgent.invite(target, options);
+  };
+
+  var bind = function (selectors, callback) {
+    elementList = document.querySelectorAll(selectors);
+    console.log(elementList);
+    Array.prototype.forEach.call(elementList, function (element) {
+      return element.addEventListener('click', function () {
+        callback(element.innerHTML);
+      });
+    }); 
   };
 
   return {
-    phone: phone
-  }
-}
+    phone: phone,
+    call: call,
+    bind: bind
+  };
+
+};
